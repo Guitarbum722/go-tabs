e47066a897e380fa683a9cdfb19c6d41b297d956
guitarbum722 <johnkenneth.moore@gmail.com>
2017-04-06 19:56:53 -0700

    Accept user input and refresh the current tablature display with their selection.

--- main.go
+++ main.go
@@ -7,30 +7,62 @@ import (
 	"strings"
 )
 
+var currentTab = map[byte]string{'E': "---", 'a': "---", 'd': "---", 'g': "---", 'b': "---", 'e': "---"}
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter the string, then fret number (ie e7 or g2)")
 	fmt.Println("****************")
 
-	// TODO(guitarbum722) create default current tablature for multiple instruments 2017-04-02T18:54 4
+	// TODO(guitarbum722) create default currentTab tablature for multiple instruments 2017-04-02T18:54 4
 	// TODO(guitarbum722) create instrument interface and instrument structs 2017-04-03T18:54 4
-	var current = map[rune]string{'E': "---", 'a': "---", 'd': "---", 'g': "---", 'b': "---", 'e': "---"}
-	for k, v := range current {
+	for k, v := range currentTab {
 		fmt.Printf("%s : %s\n", string(k), v)
 	}
 
-	fmt.Println(current)
 	for {
 		input, _ := reader.ReadString('\n')
 		input = strings.Replace(input, "\n", "", -1) // Windows; BOOO!
 
-		// Guitar string number plus frets should not be less than 2 and not more than 3 characters in length.  This allots for up to 99 frets.
-		if len(input) < 2 || len(input) > 3 {
-			fmt.Print("Invalid entry; make sure the format is [string#][fret#]")
-		}
+		guitarString, fret := parseFingerBoard(input)
 
-		// TODO(guitarbum722) parse input and reconstruct the values of current for display 2017-04-02T18:54 4
-		fmt.Println(current)
+		currentTab[guitarString] = fmt.Sprintf("-%s-", fret)
+
+		// TODO(guitarbum722) parse input and reconstruct the values of currentTab for display 2017-04-02T18:54 4
+		for k, v := range currentTab {
+			fmt.Printf("%s : %s\n", string(k), v)
+		}
 	}
+}
+
+func parseFingerBoard(i string) (byte, string) {
+
+	var guitarString byte
+	var fret string
+
+	// Guitar string number plus frets should not be less than 2 and not more than 3 characters in length.  This allots for up to 99 frets.
+	if len(i) < 2 || len(i) > 3 {
+		fmt.Print("Invalid entry; make sure the format is [string#][fret#]")
+	}
+
+	// confirms that the string input by the user is valid
+	switch i[0] {
+	case 'E':
+		guitarString = i[0]
+	case 'a':
+		guitarString = i[0]
+	case 'd':
+		guitarString = i[0]
+	case 'g':
+		guitarString = i[0]
+	case 'b':
+		guitarString = i[0]
+	case 'e':
+		guitarString = i[0]
+	}
+
+	fret = i[1:]
+
+	return guitarString, fret
 }
