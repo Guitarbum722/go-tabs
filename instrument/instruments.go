5b22171d10eff68127b510931b3470cb0807f179
guitarbum722 <johnkenneth.moore@gmail.com>
2017-04-08 19:52:24 -0700

    Add the order of the instrument strings and number of strings for the current instrument which will be used for validation upon input.

--- instrument/instruments.go
+++ instrument/instruments.go
@@ -4,20 +4,27 @@ package instrument
 type Instrument interface {
 	Tune(tuning string)
 	Fretboard() map[byte]string
-	orderTuning()
+	Order() TuningOrder
 }
+
+// TuningOrder defines the order in which the strings would be physically on the guitar.  TuningOrder can be used to display the strings properly and consistently.
+type TuningOrder []byte
 
 // Guitar represents a standard 6 string guitar with a default tuning of Eadgbe.  Tuning can be changed by calling Tune()
 type Guitar struct {
-	fretBoard map[byte]string
+	fretBoard    map[byte]string
+	order        TuningOrder
+	numOfStrings int
 }
 
 // Bass represents a standard 4 string bass guitar with a default tuning of Eadg.  Tuning can be changed by calling Tune()
 type Bass struct {
-	fretBoard map[byte]string
+	fretBoard    map[byte]string
+	order        TuningOrder
+	numOfStrings int
 }
 
-// NewInstrument returns the configured instrument in its default tuning.
+// NewInstrument returns the configured instrument in its default tuning.  A Guitar in standard tuning will be returned by default if the desired instrument is not available.
 func NewInstrument(i string) Instrument {
 	var instrument Instrument
 
@@ -33,7 +40,7 @@ func NewInstrument(i string) Instrument {
 	return instrument
 }
 
-// Tune updates the tuning configuration of the current Guitar.
+// Tune updates the tuning configuration of the current Guitar.  The order of the strings will also be the order in which the tuning was input to Tune()
 func (g *Guitar) Tune(tuning string) {
 
 }
@@ -47,6 +54,11 @@ func (g *Guitar) orderTuning() {
 
 }
 
+// Order returns the current tuning order for the current Guitar
+func (g *Guitar) Order() TuningOrder {
+	return g.order
+}
+
 // returns a pointer to a guitar with standard tuning by default.
 func newGuitar() *Guitar {
 	return &Guitar{fretBoard: map[byte]string{
@@ -56,12 +68,19 @@ func newGuitar() *Guitar {
 		'g': "---",
 		'b': "---",
 		'e': "---"},
+		order:        TuningOrder{'E', 'a', 'd', 'g', 'b', 'e'},
+		numOfStrings: 6,
 	}
 }
 
-// Tune updates the tuning configuration of the current Bass.
+// Tune updates the tuning configuration of the current Bass.  The order of the strings will also be the order in which the tuning was input to Tune()
 func (b *Bass) Tune(tuning string) {
 
+}
+
+// Order returns the current tuning order for the current Bass
+func (b *Bass) Order() TuningOrder {
+	return b.order
 }
 
 // Fretboard returns the current map which represents the Bass pointers tuning.
@@ -80,5 +99,7 @@ func newBass() *Bass {
 		'a': "---",
 		'd': "---",
 		'g': "---"},
+		order:        TuningOrder{'E', 'a', 'd', 'g'},
+		numOfStrings: 4,
 	}
 }
--- main.go
+++ main.go
@@ -1,69 +1,70 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"github.com/Guitarbum722/tablature/instrument"
+// 	"os"
+// 	"strings"
+// )
 
-// TODO(guitarbum722) define structure that will display the instrument map in order 2017-04-06T18:00 5
+// // TODO(guitarbum722) define structure that will display the instrument map in order 2017-04-06T18:00 5
 
-func main() {
-	instrument := NewInstrument("guitar").(*Guitar)
+// func main() {
+// 	instrument := instrument.NewInstrument("guitar")
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter the string, then fret number (ie e7 or g2)")
-	fmt.Println("****************")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	fmt.Println("Enter the string, then fret number (ie e7 or g2)")
+// 	fmt.Println("****************")
 
-	// TODO(guitarbum722) create default currentTab tablature for multiple instruments 2017-04-02T18:54 4
-	// TODO(guitarbum722) create instrument interface and instrument structs 2017-04-03T18:54 4
-	for k, v := range instrument.fretBoard {
-		fmt.Printf("%s : %s\n", string(k), v)
-	}
+// 	orderOfStrings := instrument.Order()
+// 	fretBoard := instrument.Fretboard()
+// 	for _, v := range orderOfStrings {
+// 		fmt.Printf("%s : %s\n", string(v), fretBoard[v])
+// 	}
 
-	for {
-		input, _ := reader.ReadString('\n')
-		input = strings.Replace(input, "\n", "", -1) // Windows; BOOO!
+// 	for {
+// 		input, _ := reader.ReadString('\n')
+// 		input = strings.Replace(input, "\n", "", -1) // Windows; BOOO!
 
-		guitarString, fret := parseFingerBoard(input)
+// 		guitarString, fret := parseFingerBoard(input)
 
-		currentTab[guitarString] = fmt.Sprintf("-%s-", fret)
+// 		fretBoard[guitarString] = fmt.Sprintf("-%s-", fret)
 
-		// TODO(guitarbum722) parse input and reconstruct the values of currentTab for display 2017-04-02T18:54 4
-		for k, v := range currentTab {
-			fmt.Printf("%s : %s\n", string(k), v)
-		}
-	}
-}
+// 		// TODO(guitarbum722) parse input and reconstruct the values of currentTab for display 2017-04-02T18:54 4
+// 		for _, v := range orderOfStrings {
+// 			fmt.Printf("%s : %s\n", string(v), fretBoard[v])
+// 		}
+// 	}
+// }
 
-func parseFingerBoard(i string) (byte, string) {
+// func parseFingerBoard(i string) (byte, string) {
 
-	var guitarString byte
-	var fret string
+// 	var guitarString byte
+// 	var fret string
 
-	// Guitar string number plus frets should not be less than 2 and not more than 3 characters in length.  This allots for up to 99 frets.
-	if len(i) < 2 || len(i) > 3 {
-		fmt.Print("Invalid entry; make sure the format is [string#][fret#]")
-	}
+// 	// Guitar string number plus frets should not be less than 2 and not more than 3 characters in length.  This allots for up to 99 frets.
+// 	if len(i) < 2 || len(i) > 3 {
+// 		fmt.Print("Invalid entry; make sure the format is [string#][fret#]")
+// 	}
 
-	// confirms that the string input by the user is valid
-	switch i[0] {
-	case 'E':
-		guitarString = i[0]
-	case 'a':
-		guitarString = i[0]
-	case 'd':
-		guitarString = i[0]
-	case 'g':
-		guitarString = i[0]
-	case 'b':
-		guitarString = i[0]
-	case 'e':
-		guitarString = i[0]
-	}
+// 	// confirms that the string input by the user is valid
+// 	switch i[0] {
+// 	case 'E':
+// 		guitarString = i[0]
+// 	case 'a':
+// 		guitarString = i[0]
+// 	case 'd':
+// 		guitarString = i[0]
+// 	case 'g':
+// 		guitarString = i[0]
+// 	case 'b':
+// 		guitarString = i[0]
+// 	case 'e':
+// 		guitarString = i[0]
+// 	}
 
-	fret = i[1:]
+// 	fret = i[1:]
 
-	return guitarString, fret
-}
+// 	return guitarString, fret
+// }
