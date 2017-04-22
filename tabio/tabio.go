6026350ae13d67e7ce28c1fb04d8abce03abc960
guitarbum722 <johnkenneth.moore@gmail.com>
2017-04-22 14:38:47 -0700

    Remove error returned from StageTablature()

--- tabio/tabio.go
+++ tabio/tabio.go
@@ -36,16 +36,18 @@ func NewTablatureWriter(w io.Writer, pos int) *TablatureWriter {
 // StageTablature writes the Instrument's current tablature to w for buffering.
 // The purpose is only to stage or buffer the current tablature but it does not
 // write the tablature to a file.
-func StageTablature(i instrument.Instrument, w *TablatureWriter) error {
+func StageTablature(i instrument.Instrument, w *TablatureWriter) {
 
 	for k, v := range i.Fretboard() {
+		// remove
+		fmt.Println("Len of tb.builder[k] : ", len(w.tb.builder[k]))
 		if len(w.tb.builder[k]) == 0 {
 			w.tb.builder[k] = append(w.tb.builder[k], k, ':', ' ')
 		}
 		w.tb.builder[k] = append(w.tb.builder[k], []byte(v)...)
 	}
 
-	return nil
+	return
 }
 
 // ExportTablature will flush the bufferred writer to the io.Writer of which it was initialized
