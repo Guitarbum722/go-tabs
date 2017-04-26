e402134eb8a9dfcf7d6738daea01966d80e5d8e5
guitarbum722 <johnkenneth.moore@gmail.com>
2017-04-25 19:51:15 -0700

    Add a method to update the wrapPosition of the TablatureWriter and make the wrapPosition unexported

--- tabio/tabio.go
+++ tabio/tabio.go
@@ -12,13 +12,23 @@ import (
 // 20 is the default.
 type TablatureWriter struct {
 	*bufio.Writer
-	WrapPosition int
+	wrapPosition int
 	totalLength  int
 	tb           tablatureBuilder
 }
 
 type tablatureBuilder struct {
 	builder map[byte][]byte
+}
+
+// UpdateWrapPosition will update the wrapPosition of the TablatureWriter
+// which will have a default value of 20 if the input is less than 20 for
+// readability
+func (tw *TablatureWriter) UpdateWrapPosition(pos int) {
+	if pos < 20 {
+		pos = 20
+	}
+	tw.wrapPosition = pos
 }
 
 // NewTablatureWriter creates a buffered writer to be used for staging tablature
@@ -55,14 +65,14 @@ func ExportTablature(i instrument.Instrument, w *TablatureWriter) error {
 
 	var done int
 
-	for ; done < w.totalLength; done += w.WrapPosition {
+	for ; done < w.totalLength; done += w.wrapPosition {
 
 		for _, v := range i.Order() {
 
 			w.Write([]byte{v, ':', ' '})
 
-			if (done + w.WrapPosition) < w.totalLength {
-				if _, err := w.Write(w.tb.builder[v][done:(done + w.WrapPosition)]); err != nil {
+			if (done + w.wrapPosition) < w.totalLength {
+				if _, err := w.Write(w.tb.builder[v][done:(done + w.wrapPosition)]); err != nil {
 					return errors.Wrap(err, "write to buffer failed")
 				}
 			} else {
