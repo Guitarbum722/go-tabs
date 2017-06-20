aa70d76317902e9d5c3bf6ded7e42e5d15109ad3
guitarbum722 <johnkenneth.moore@gmail.com>
2017-06-20 16:55:10 -0700

    add utility func to append space after the label of the instrument's for proper alignment when being displayed or exported

--- tabio/tabio.go
+++ tabio/tabio.go
@@ -2,6 +2,7 @@ package tabio
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 
 	"github.com/Guitarbum722/go-tabs/instrument"
@@ -19,7 +20,7 @@ type TablatureWriter struct {
 }
 
 type tablatureBuilder struct {
-	builder map[byte][]byte
+	builder map[string][]byte
 }
 
 // UpdateWrapPosition will update the wrapPosition of the TablatureWriter
@@ -42,7 +43,7 @@ func NewTablatureWriter(w io.Writer, pos int) *TablatureWriter {
 		pos,
 		0,
 		tablatureBuilder{
-			make(map[byte][]byte),
+			make(map[string][]byte),
 		},
 	}
 }
@@ -51,9 +52,7 @@ func NewTablatureWriter(w io.Writer, pos int) *TablatureWriter {
 // The purpose is only to stage or buffer the current tablature but it does not
 // write the tablature to a file.
 func StageTablature(i instrument.Instrument, w *TablatureWriter) {
-
 	for k, v := range i.Fretboard() {
-
 		w.tb.builder[k] = append(w.tb.builder[k], []byte(v)...)
 		w.totalLength = len(w.tb.builder[k])
 	}
@@ -63,14 +62,10 @@ func StageTablature(i instrument.Instrument, w *TablatureWriter) {
 
 // ExportTablature will flush the bufferred writer to the io.Writer of which it was initialized
 func ExportTablature(i instrument.Instrument, w *TablatureWriter) error {
-
 	var done int
-
 	for ; done < w.totalLength; done += w.wrapPosition {
-
 		for _, v := range i.Order() {
-
-			w.Write([]byte{v, ':', ' '})
+			w.WriteString(padLabel(v) + ":" + " ")
 
 			if (done + w.wrapPosition) < w.totalLength {
 				if _, err := w.Write(w.tb.builder[v][done:(done + w.wrapPosition)]); err != nil {
@@ -89,4 +84,22 @@ func ExportTablature(i instrument.Instrument, w *TablatureWriter) error {
 	w.Flush()
 
 	return nil
+}
+
+// StringifyCurrentTab converts the current fretBoard configuration to a string.
+func StringifyCurrentTab(i instrument.Instrument) string {
+	orderOfStrings := i.Order()
+	fretBoard := i.Fretboard()
+	var result string
+	for _, v := range orderOfStrings {
+		result += fmt.Sprintf("%v : %v\n", padLabel(v), fretBoard[v])
+	}
+	return result
+}
+
+func padLabel(s string) string {
+	for len(s) < 2 {
+		s += " "
+	}
+	return s
 }
